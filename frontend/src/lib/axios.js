f68956d6224439c749c2f1821840e9e57c1d9587
AlinaPathan <alinapathan2004@gmail.com>
2026-06-16 12:12:29 +0530

    final commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://campusclique.onrender.com",
+  baseURL: "https://campusclique.onrender.com/api",
   withCredentials: true,
 });
